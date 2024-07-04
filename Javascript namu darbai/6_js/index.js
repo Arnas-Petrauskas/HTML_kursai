1c9594ef2e6cc8cff0cea054559829a1b2ed3c59
Arnas-Petrauskas <142972378+Arnas-Petrauskas@users.noreply.github.com>
2024-07-04 15:01:11 +0300

    Add files via upload

---
+++ Javascript namu darbai/6_js/index.js
@@ -0,0 +1,17 @@
+document.querySelector("#email").addEventListener("submit", function(event){
+    event.preventDefault();
+    f = "a";
+    pyramid();
+    })
+
+function pyramid(){
+    x = document.querySelector("#number").value;
+    const ul_item = document.querySelector("ul");
+    for (i = 1; i <= x; i++) {
+        let li_item = document.createElement("li");
+        let text = document.createTextNode("*".repeat(i));
+        li_item.appendChild(text);
+        ul_item.appendChild(li_item);
+    }
+
+}
